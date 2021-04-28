c83d8a8fbe940a311e9c489cfd7061953682d3b1
Elvira Bolshakova <elyabolshakova18085@gmail.com>
2021-04-28 19:27:43 +1000

    c

--- functions.h
+++
@@ -1,8 +0,0 @@
-
-void printhelp();
-void printerror();
-void VVOD(int a[100],const int N);
-double average(int a[100],const int N);
-double variance(int a[100],const int N);
-double dispersia(int a[100],const int N);
-
